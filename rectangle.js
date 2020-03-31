beda5a6a89d191f3139a1fa97fef7e85a68c770e
luyishuang <1781990652@qq.com>
2020-03-31 23:02:15 +0800

    v0.3

--- rectangle.js
+++ rectangle.js
@@ -1,28 +1,18 @@
-$(function(){
-    //get dom elem
+/* global Rectangle: true */
+$(function() {
     var $width = $('#width'),
         $height = $('#height'),
         $btnCal = $('#calculate'),
         $perimeter = $('#perimeter'),
         $area = $('#area');
-
-    /*calculate button click event*/
-    $btnCal.click(function () {
-        //get value
-        var w = Number($width.val()),
-            h = Number($height.val());
-
-        //calculate
-            var p = roundFractional(2 *(w + h), 2),
-                a = roundFractional(w * h, 2);
-
-        //output
-        $perimeter.val(p);
-        $area.val(a);
+  
+    $btnCal.click(function(){
+      var w = $width.val(),
+          h = $height.val();
+  
+      var r = new Rectangle(w, h);
+  
+      $perimeter.val(r.perimeter());
+      $area.val(r.area());
     });
-
-    function roundFractional(x, n) {
-        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-      }
-
-});
+  });
